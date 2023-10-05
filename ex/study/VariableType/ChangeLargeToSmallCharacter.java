55e6a0e2d5cf1ca6cfa4ad29e9a112bdc5339718
Jiwon <kimg1623@naver.com>
2023-10-05 10:28:14 +0900

    변수 타입 실습

---
+++ ex/study/VariableType/ChangeLargeToSmallCharacter.java
@@ -0,0 +1,13 @@
+package ex.study.VariableType;
+
+public class ChangeLargeToSmallCharacter {
+    public static void main(String[] args) {
+
+        /* 변수ch가 대문자인 경우에만 소문자로 변경 */
+
+        char ch = 'A';
+        char lowerCase = ('A'<=ch&&ch<='Z') ? (char)(ch+32) : ch;
+        System.out.println("ch:"+ch);
+        System.out.println("ch to lowerCase:"+lowerCase);
+    }
+}
