3d826bff2c23f5c278b99d5980ac2b6ca182c515
Jiwon <kimg1623@naver.com>
2023-10-05 10:27:45 +0900

    연산자 실습

---
+++ ex/study/Operator/ChangeNumAlphabet.java
@@ -0,0 +1,11 @@
+package ex.study.Operator;
+
+public class ChangeNumAlphabet {
+    public static void main(String[] args) {
+
+        /* 1의 자리를 무조건 1로 바꿈 */
+
+        int num = 333;
+        System.out.println(num / 10 * 10 + 1);
+    }
+}
