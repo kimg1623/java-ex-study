8ffd86fc5f25d1fe97ea43a56fdbb807586804d2
Jiwon <kimg1623@naver.com>
2023-10-10 16:52:32 +0900

    배열 실습1

---
+++ 04-Array/CountCoinForChange.java
@@ -0,0 +1,18 @@
+public class CountCoinForChange {
+    /***
+     * 요약: 거스름돈을 위해 각각 몇개의 동전이 필요한지 계산
+     * 조건: 나눗셈 연산자와 나머지 연산자를 사용
+     */
+    public static void main(String[] args) {
+        // 큰 금액의 동전을 우선적으로 거슬러 줘야한다.
+        int[] coinUnit = {500, 100, 50, 10};
+
+        int money = 2680;
+        System.out.println("money="+money);
+
+        for(int i=0;i<coinUnit.length;i++) {
+            System.out.println(coinUnit[i]+"원: "+money / coinUnit[i]);
+            money %= coinUnit[i];
+        }
+    }
+}
---
+++ 04-Array/DrawingStars.java
@@ -0,0 +1,22 @@
+public class DrawingStars {
+    /***
+     * 요약: 배열 answer에 담긴 데이터를 읽고 각 숫자의 갯수를 세어서 *로 그래프 만들기
+     * answer는 1~4까지의 정수가 있는 배열
+     */
+    public static void main(String[] args) {
+        int[] answer = {1, 4, 4, 3, 1, 4, 4, 2, 1, 3, 2};
+        int[] counter = new int[4];
+
+        for (int i = 0; i < answer.length; i++) {
+            counter[answer[i] - 1]++;
+            // counter 위치에 answer값을 넣어야 함, 배열은 0부터 시작하므로 -1
+        }
+
+        for (int i = 0; i < counter.length; i++) {
+            System.out.print(counter[i]);
+            for (int j = 0; j < counter[i]; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+}
